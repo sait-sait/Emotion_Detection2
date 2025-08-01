ee74a631faaf31b813cb9eed209faacf0a2bd937
unknown <mohammed.hamzasait2024@vitstudent.ac.in>
2025-08-01 20:23:05 +0530

    Added params.yaml for hyperparameter tuning

--- src/data/data_ingestion.py
+++ src/data/data_ingestion.py
@@ -2,7 +2,14 @@ import pandas as pd
 import numpy as np
 import os
 
+import yaml
+
 from sklearn.model_selection import train_test_split
+
+with open("params.yaml","r") as file:
+    params = yaml.safe_load(file)
+
+test_size = params['data_ingestion']['test_size']
 
 df = pd.read_csv('https://raw.githubusercontent.com/campusx-official/jupyter-masterclass/main/tweet_emotions.csv')
 
@@ -10,7 +17,7 @@ final_df = df[df['sentiment'].isin(['happiness','sadness'])]
 
 final_df['sentiment'].replace({'happiness':1, 'sadness':0},inplace=True)
 
-train_data, test_data = train_test_split(final_df, test_size=0.2, random_state=42)
+train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=42)
 
 os.makedirs('data/raw',exist_ok=True)
 
--- src/models/modelling.py
+++ src/models/modelling.py
@@ -2,15 +2,21 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
-
+import yaml
 from sklearn.ensemble import RandomForestClassifier
+
+with open("params.yaml","r") as file:
+    params = yaml.safe_load(file)
+
+n_estimators = params['modelling']['n_estimators']
+max_depth = params['modelling']['max_depth']
 
 train_data = pd.read_csv("data/interim/train_bow.csv")
 
 x_train = train_data.drop(columns=['label']).values
 y_train = train_data['label'].values
 
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=n_estimators,max_depth=max_depth, random_state=42)
 model.fit(x_train, y_train)
 
 pickle.dump(model, open("models/random_forest_model.pkl", "wb"))
